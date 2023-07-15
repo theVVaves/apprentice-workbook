229cffcef41eeee7900732b2e1fa20180f2638c3
war and peaches <th3w4v35@protonmail.ch>
2023-07-15 00:21:56 -0400

    one more alternate set

--- content/challenges/medium/2 - Missing Number/func.js
+++ content/challenges/medium/2 - Missing Number/func.js
@@ -3,8 +3,35 @@
 //   - The array of numbers will be unsorted (not in order).
 //   - Only one number will be missing.
 module.exports = (arr) => {
-    // Calculate the sum of all numbers from 1 to 10
-    const expectedSum = 55;
+    /*     const numberSet = new Set(arr); // Create a Set from the array
+    
+        // Iterate through the numbers from 1 to 10
+        for (let i = 1; i <= 10; i++) {
+            if (!numberSet.has(i)) {
+                return i; // Found the missing number
+            }
+        }
+    
+        return -1; // Return -1 if no missing number is found
+     */
+
+    /*     let missingNumber = 0;
+      
+        // XOR all the numbers from 1 to 10
+        for (let i = 1; i <= 10; i++) {
+          missingNumber ^= i;
+        }
+        
+        // XOR all the numbers in the array
+        for (let num of arr) {
+          missingNumber ^= num;
+        }
+        
+        return missingNumber;
+     */
+
+    const n = 10; // Total numbers from 1 to 10
+    const expectedSum = (n * (n + 1)) / 2; // Sum of consecutive numbers from 1 to 10
 
     // Calculate the actual sum of the given array
     const actualSum = arr.reduce((sum, num) => sum + num, 0);
@@ -13,4 +40,5 @@ module.exports = (arr) => {
     const missingNumber = expectedSum - actualSum;
 
     return missingNumber;
+
 }
